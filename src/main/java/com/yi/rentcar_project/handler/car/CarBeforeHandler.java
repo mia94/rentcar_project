ceb4efdc869761753a72f6cbdb574c4055921ad4
mia94 <mia_3@hanmail.net>
2019-01-10 14:10:52 +0900

    carBefore핸들러 생성

---
+++ src/main/java/com/yi/rentcar_project/handler/car/CarBeforeHandler.java
@@ -0,0 +1,16 @@
+package com.yi.rentcar_project.handler.car;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yi.rentcar_project.mvc.CommandHandler;
+
+public class CarBeforeHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		return null;
+	}
+
+}
