9f3147f79a9c6f91d4def4bcd6b34387c46d6228
mia94 <mia_3@hanmail.net>
2019-01-10 19:33:16 +0900

    carread 다음차량 조회완료

--- src/main/java/com/yi/rentcar_project/handler/car/CarAfterForEmpHandler.java
+++ src/main/java/com/yi/rentcar_project/handler/car/CarAfterForEmpHandler.java
@@ -3,29 +3,25 @@ package com.yi.rentcar_project.handler.car;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.yi.rentcar_project.model.CarModel;
 import com.yi.rentcar_project.mvc.CommandHandler;
 import com.yi.rentcar_project.service.CarModelService;
 
-public class CarBeforeForEmpHandler implements CommandHandler {
+public class CarAfterForEmpHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		String code = req.getParameter("carCode");
-		String sCode = code.substring(1);
-		int num = Integer.parseInt(sCode) - 1;//숫자로 출력 앞에 0 사라짐
-		String beforeCode = String.format("V%03d", num);
-		
-		CarModel model = new CarModel();
-		model.setCarCode(beforeCode);
-		
+		//해당모델이 없으면 원래화면 유지
 		CarModelService service = CarModelService.getInstance();
-		if(beforeCode.equals("V000")){
+		String maxCode = service.nextCarCode();
+		if(code.equals(maxCode)){
 			return "carread.do?carCode="+code;
 		}
-		model = service.selectCarModelByNo(model);
 		
-		return "carread.do?carCode="+beforeCode;
+		String sCode = code.substring(1);
+		int num = Integer.parseInt(sCode) + 1;//숫자로 출력 앞에 0 사라짐
+		String afterCode = String.format("V%03d", num);
+		return "carread.do?carCode="+afterCode;
 	}
 
 }
