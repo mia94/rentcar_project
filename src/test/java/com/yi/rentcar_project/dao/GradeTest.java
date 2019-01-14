b6b363600c40e7b1fc503bdc37b099da0b6f7562
psw2701 <psw2701@naver.com>
2019-01-14 12:53:41 +0900

    commit

---
+++ src/test/java/com/yi/rentcar_project/dao/GradeTest.java
@@ -0,0 +1,22 @@
+package com.yi.rentcar_project.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.yi.rentcar_project.model.Grade;
+import com.yi.rentcar_project.service.GradeService;
+
+public class GradeTest {
+	private GradeDao dao = GradeService.getInstance();
+	@Test
+	public void test01selectGradeByAll() {
+		List<Grade> gradList = dao.selectGradeByAll();
+		System.out.println(gradList);
+		Assert.assertNotNull(gradList);
+	}
+
+}
