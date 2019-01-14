3ed70f8c141da8f5696474f8e4738cc13b9fe514
mia94 <mia_3@hanmail.net>
2019-01-14 17:54:00 +0900

    렌트 카운트 테스트완료

--- src/main/java/com/yi/rentcar_project/dao/RentDao.java
+++ src/main/java/com/yi/rentcar_project/dao/RentDao.java
@@ -3,9 +3,12 @@ package com.yi.rentcar_project.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.yi.rentcar_project.model.CarModel;
 import com.yi.rentcar_project.model.Customer;
 import com.yi.rentcar_project.model.Insurance;
+import com.yi.rentcar_project.model.StateCar;
 
 public interface RentDao {
 	//검색 -> 조건에 소형 입력해서 소형에 해당하는 차량들의 이름, 브랜드, 연료종류 가지고오기
@@ -16,4 +19,8 @@ public interface RentDao {
 	
 	//보험
 	public Insurance selectInsuranceByCarType(String carType) throws SQLException;
+	
+	//브랜드별 카운트 select
+	@Select("SELECT DATE_FORMAT(start_date,'%Y-%m') as title, b.name as brand, COUNT(*) as count FROM rent r join car_model cm on r.car_code = cm.car_code join brand b on cm.brand = b.no GROUP BY title, brand having b.name = #{brand}")
+	List<StateCar> selectCountRentByMonthWithBrand(String brand);
 }
--- src/main/java/com/yi/rentcar_project/handler/carchart/CarLineChartHandler.java
+++ src/main/java/com/yi/rentcar_project/handler/carchart/CarLineChartHandler.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yi.rentcar_project.mvc.CommandHandler;
+import com.yi.rentcar_project.service.StateCarService;
 
 public class CarLineChartHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		// TODO Auto-generated method stub
+		StateCarService service = StateCarService.getInstance();
+		
 		return "/WEB-INF/view/chart/carlinechart.jsp";
 	}
 
